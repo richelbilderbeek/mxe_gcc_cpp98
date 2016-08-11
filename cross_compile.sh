8784172da1d457c2fac038935b933ddea6f950cc
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-11 13:47:26 +0200

    An exe does not have .exe at the end when crosscompiling?

--- cross_compile.sh
+++ cross_compile.sh
@@ -3,6 +3,7 @@
 export PATH=`pwd`/mxe/usr/bin:$PATH
 my_gcc="i686-w64-mingw32.static-g++"
 my_folder="travis_gcc_cpp98"
+my_exe=$my_folder # Note: no .exe added
 
 echo "Displaying cross compiler version (if it is not found, set the path to YourFolder/mxe/usr/bin"
 $my_gcc -v
@@ -10,9 +11,10 @@ $my_gcc -v
 cd $my_folder
 $my_gcc main.cpp -o $my_folder
 
-if [ ! -e ./release/$my_folder".exe" ]
+if [ ! -e $my_exe ]
 then
   echo "FAIL (executable not found)"
+  echo $my_exe
   return 1
 fi
 
