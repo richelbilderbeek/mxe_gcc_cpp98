58e7a0a16bae689fb68768392b3fbf31657759c8
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-24 13:00:31 +0200

    Use intltool

--- install_mxe.sh
+++ install_mxe.sh
@@ -7,7 +7,7 @@ echo "Install the MXE prerequisites"
 # p7zip-full   | 7za
 
 # Must be one line!
-sudo apt install p7zip-full autopoint gettext bison flex gperf intltoolize libtool p7zip-full ruby scons
+sudo apt install p7zip-full autopoint gettext bison flex gperf intltool libtool p7zip-full ruby scons
 
 echo "Clone the GitHub"
 git clone https://github.com/mxe/mxe.git
